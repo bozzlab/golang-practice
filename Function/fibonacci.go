2f83e295d471353d220fdabbfaa659c5d883b20b
Bozz <peem@liveup.cc>
2019-07-03 10:55:04 +0700

    refactor all

--- Function/fibonacci.go
+++ Function/fibonacci.go
@@ -4,18 +4,17 @@ import "fmt"
 
 // fibonacci is a function that returns
 // a function that returns an int.
-func fibonacci() func(int) int {
-	y := 0
-	return func(x int) int {
-		a := x + y
-		y = x
-		return a
+func fibonacci() func() int {
+	x, y := -1, 1
+	return func() int {
+		x, y = y, x+y
+		return y
 	}
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f(i))
+		fmt.Println(f())
 	}
 }
